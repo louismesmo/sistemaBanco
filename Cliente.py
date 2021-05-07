e1fb97449aba8b373a2f349b115206c05661a528
Louis <henrique.louisvr@gmail.com>
2021-05-06 21:54:23 -0300

    Initial Commit
    pre-release commit

---
+++ Cliente.py
@@ -0,0 +1,5 @@
+class Cliente:
+    def __init__(self, nome, cpf, endereco):
+        self.nome = nome
+        self.cpf = cpf
+        self.endereco = endereco
---
+++ Conta.py
@@ -0,0 +1,38 @@
+import datetime
+from Extrato import Extrato
+
+class Conta:
+    def __init__(self, numero, clientes, saldo):
+        self.numero = numero
+        self.clientes = clientes
+        self.saldo = saldo
+        self.abertura = datetime.datetime.today()
+        self.extrato = Extrato()
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.extrato.transacoes.append(["DEPOSITO", valor, "Data", datetime.datetime.today()])
+
+    def sacar(self, valor):
+
+        if self.saldo < valor:
+            return False
+
+        else:
+            self.saldo -= valor
+            self.extrato.transacoes.append(["SAQUE", valor, "Data", datetime.datetime.today()])
+            return True
+    #Realiza a transferencia de saldo entre contas
+    def pix(self, contaDestino, valor):
+        if self.saldo < valor:
+            return ("Você não possui saldo suficiente.")
+
+        else:
+            self.saldo -= valor
+            contaDestino += valor
+            self.extrato.transacoes.append(["TRANSFERENCIA", valor, "Data", datetime.datetime.today()])
+            return ("Transação realizada com sucesso!")
+
+    #Gera o saldo atual da conta
+    def gerarsaldo(self):
+        print(f'numero: {self.numero}\nsaldo: {self.saldo}')
